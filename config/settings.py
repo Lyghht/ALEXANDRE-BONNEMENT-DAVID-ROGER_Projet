5e0ce9f87af93f73ac8b589acb92475b37871abf
Simon Roger <simon.roger@etud.u-picardie.fr>
2025-01-07 18:43:09 +0100

    Améliorations des settings

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,12 @@ class Config:
     def __init__(self):
         self.screenWidth = 800
         self.screenHeight = 600
+        self.bgColor = (0, 0, 0)  # Noir
+        self.fps = 60
+        self.paddleSpeed = 5
         self.ballSpeed = 4
         self.colors = {
+            "brick": (255, 0, 0),  # Rouge
+            "paddle": (255, 255, 255),  # Blanc
             "ball": (0, 255, 0),  # Vert
         }
