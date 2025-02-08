2dd8a820baea3d38ca370495fc7e32e84c3f4e9a
Simon Roger <simon.roger@etud.u-picardie.fr>
2025-02-08 14:54:58 +0100

    Amélioration levelLoader

--- levels/levelLoader.py
+++ levels/levelLoader.py
@@ -1,14 +1,12 @@
 import json
 from entities.brick import Brick
 
-
-"""
-Fonction permettant de charger un niveau depuis un fichier JSON
-@param config: Configuration du jeu
-@param level_file: Fichier JSON contenant les informations du niveau
-@return: Liste de briques
-"""
 def loadLevel(config, layout):
+    """
+        Fonction qui permet de charger un niveau
+        @param config: Configuration du jeu
+        @return: Liste de briques
+    """
     bricks = [] #Permet de créer une liste de bricks vide, c'est elle qui va contenir les briques du niveau
     numColumns = len(layout[0]) #Nombre de colonnes sur la grille
     brickWidth = config.screenWidth // numColumns #Division entière pour obtenir la largeur des briques
@@ -16,7 +14,7 @@ def loadLevel(config, layout):
 
     for rowIndex, row in enumerate(layout): #Parcours de chaque ligne de la grille
         for colIndex, cell in enumerate(row): #Parcours de chaque colonne de la grille
-            if cell > 0:  # 1 représente une brique dans la grille (du json)
+            if cell > 0:
                 x = colIndex * brickWidth
                 y = rowIndex * brickHeight + config.screenHudHeight
                 bricks.append(Brick(x, y, brickWidth, brickHeight, cell, config)) #Ajout de la brique à la liste de briques
