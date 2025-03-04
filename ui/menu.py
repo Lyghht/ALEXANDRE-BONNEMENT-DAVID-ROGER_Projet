2fb0ded67e3fd6889228e0b990a8e62eb22f9373
LeoB <leo.bonnement@cmc-dev.com>
2025-03-04 18:41:50 +0100

    Changement nom de variables en camelCase dans menu.py

--- ui/menu.py
+++ ui/menu.py
@@ -12,15 +12,15 @@ class Menu:
         Police de caractères
     title :
         Titre du jeu
-    title_rect : pygame.Rect
+    titleRect : pygame.Rect
         Rectangle contenant le titre
     play :
         Bouton pour lancer le jeu
-    play_rect : pygame.Rect
+    playRect : pygame.Rect
         Rectangle contenant le bouton pour lancer le jeu
     quit :
         Bouton pour quitter le jeu
-    quit_rect : pygame.Rect
+    quitRect : pygame.Rect
         Rectangle contenant le bouton pour quitter le jeu
     """
     def __init__(self, config):
@@ -35,13 +35,13 @@ class Menu:
 
         # Titre
         self.title = self.fontTitle.render("Casse Brique", True, (255, 255, 255))
-        self.title_rect = self.title.get_rect(center=(config.screenWidth // 2, 100))
+        self.titleRect = self.title.get_rect(center=(config.screenWidth // 2, 100))
 
         # Création des boutons avec padding
         self.createButton("Jouer", config.screenHeight // 2)
-        self.createButton("Quitter", config.screenHeight - 70, is_quit=True)
+        self.createButton("Quitter", config.screenHeight - 70, isQuit=True)
 
-    def createButton(self, text, y_pos, is_quit=False):
+    def createButton(self, text, yPos, isQuit=False):
         """
         Crée un bouton avec le texte centré
 
@@ -49,37 +49,37 @@ class Menu:
         ----------
         text : str
             Texte du bouton
-        y_pos : int
+        yPos : int
             Position verticale du bouton
-        is_quit : bool
+        isQuit : bool
             Indique si c'est le bouton quitter
         """
         # Création du texte
-        if is_quit:
-            rendered_text = self.font.render(text, True, (100, 100, 100))
+        if isQuit:
+            renderedText = self.font.render(text, True, (100, 100, 100))
         else:
-            rendered_text = self.fontPlay.render(text, True, (100, 100, 100))
-        text_rect = rendered_text.get_rect()
+            renderedText = self.fontPlay.render(text, True, (100, 100, 100))
+        textRect = renderedText.get_rect()
         
         # Création du rectangle du bouton avec padding
-        button_rect = pygame.Rect(0, 0, 
-                                text_rect.width + self.padding[0] * 2,
-                                text_rect.height + self.padding[1] * 2)
+        buttonRect = pygame.Rect(0, 0,
+                                textRect.width + self.padding[0] * 2,
+                                textRect.height + self.padding[1] * 2)
         
         # Centrage du rectangle du bouton
-        button_rect.center = (self.config.screenWidth // 2, y_pos)
+        buttonRect.center = (self.config.screenWidth // 2, yPos)
         
         # Centrage du texte dans le rectangle du bouton
-        text_rect.center = button_rect.center
+        textRect.center = buttonRect.center
 
-        if is_quit:
-            self.quit = rendered_text
-            self.quit_rect = button_rect
-            self.quit_text_rect = text_rect
+        if isQuit:
+            self.quit = renderedText
+            self.quitRect = buttonRect
+            self.quit_textRect = textRect
         else:
-            self.play = rendered_text
-            self.play_rect = button_rect
-            self.play_text_rect = text_rect
+            self.play = renderedText
+            self.playRect = buttonRect
+            self.play_textRect = textRect
 
     def draw(self, screen):
         """
@@ -91,13 +91,13 @@ class Menu:
             Surface sur laquelle dessiner le menu
         """
         # Dessiner le titre
-        screen.blit(self.title, self.title_rect)
+        screen.blit(self.title, self.titleRect)
 
         # Dessiner les boutons
-        self.drawButton(screen, self.play_rect, self.play, self.play_text_rect)
-        self.drawButton(screen, self.quit_rect, self.quit, self.quit_text_rect)
+        self.drawButton(screen, self.playRect, self.play, self.play_textRect)
+        self.drawButton(screen, self.quitRect, self.quit, self.quit_textRect)
 
-    def drawButton(self, screen, button_rect, text, text_rect):
+    def drawButton(self, screen, buttonRect, text, textRect):
         """
         Dessine un bouton avec son texte centré
 
@@ -105,16 +105,16 @@ class Menu:
         ----------
         screen : pygame.Surface
             Surface sur laquelle dessiner
-        button_rect : pygame.Rect
+        buttonRect : pygame.Rect
             Rectangle du bouton
         text : pygame.Surface
             Surface contenant le texte
-        text_rect : pygame.Rect
+        textRect : pygame.Rect
             Rectangle du texte
         """
-        pygame.draw.rect(screen, (255, 255, 255), button_rect, border_radius=10)
-        pygame.draw.rect(screen, (0, 0, 0), button_rect, 2, border_radius=10)
-        screen.blit(text, text_rect)
+        pygame.draw.rect(screen, (255, 255, 255), buttonRect, border_radius=10)
+        pygame.draw.rect(screen, (0, 0, 0), buttonRect, 2, border_radius=10)
+        screen.blit(text, textRect)
 
     def handleEvent(self, event):
         """
@@ -126,16 +126,16 @@ class Menu:
             Evénement à gérer
         """
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.play_rect.collidepoint(event.pos):
+            if self.playRect.collidepoint(event.pos):
                 return "play"
-            elif self.quit_rect.collidepoint(event.pos):
+            elif self.quitRect.collidepoint(event.pos):
                 return "quit"
         if event.type == pygame.MOUSEMOTION:
-            if self.play_rect.collidepoint(event.pos):
+            if self.playRect.collidepoint(event.pos):
                 self.play = self.fontPlay.render("Jouer", True, (0, 0, 0))
             else:
                 self.play = self.fontPlay.render("Jouer", True, (100, 100, 100))
-            if self.quit_rect.collidepoint(event.pos):
+            if self.quitRect.collidepoint(event.pos):
                 self.quit = self.font.render("Quitter", True, (0, 0, 0))
             else:
                 self.quit = self.font.render("Quitter", True, (100, 100, 100))
